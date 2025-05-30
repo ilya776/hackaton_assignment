83a2a851e8b6ea34e352b22dcf859fc79d3514ef
Illia Bilous <krabbys1111@gmail.com>
2025-05-30 13:40:29 +0300

    bug fixes

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -52,9 +52,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'backend.accounts',
+    'accounts',
     'corsheaders',
-    'backend.books',
+    'books',
 ]
 
 MIDDLEWARE = [
